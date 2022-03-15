aa1e5650d4467eb5214dd1d358a6bfbe7655cc59
TCKACHIKSIS <71429999+TCKACHIKSIS@users.noreply.github.com>
2022-03-15 14:14:37 +1000

    upd cmanelist

--- MainWindow/MainWindow.cpp
+++ MainWindow/MainWindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
 	, ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
-  this->setWindowTitle("DSP-Ткаченко");
+  this->setWindowTitle("DSP-Ткаченко, Пограничный, Семенов, Смирнов");
 }
 
 MainWindow::~MainWindow()
