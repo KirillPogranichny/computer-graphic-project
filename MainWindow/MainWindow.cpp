7540266ff35c285f98029279191ea1604ea49dad
TCKACHIKSIS <71429999+TCKACHIKSIS@users.noreply.github.com>
2022-03-21 14:01:10 +1000

    add open file window and class FileHandler

--- MainWindow/MainWindow.cpp
+++ MainWindow/MainWindow.cpp
@@ -1,6 +1,10 @@
 #include "Mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QMessageBox>
+#include <QFileDialog>
+#include <string>
+#include <iostream>
+#include <fstream>
+
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 	, ui(new Ui::MainWindow)
@@ -8,6 +12,7 @@ MainWindow::MainWindow(QWidget *parent)
   ui->setupUi(this);
   this->setWindowTitle("DSP-Ткаченко, Пограничный, Семенов, Смирнов");
   connect(this->ui->fileOpen, &QAction::triggered, this, &MainWindow::on_fileOpen_triggered);
+  connect(this->ui->fileSave, &QAction::triggered, this, &MainWindow::on_fileSave_triggered);
 }
 
 MainWindow::~MainWindow()
@@ -17,14 +22,11 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_fileOpen_triggered()
 {
-	QMessageBox *msg = new QMessageBox;
-	msg->setText("FileOpen");
-	msg->exec();
+	QString data = QFileDialog::getOpenFileName(0, "Выберите файл", "/");
+
 }
 
 void MainWindow::on_fileSave_triggered() {
-  	QMessageBox *msg = new QMessageBox;
-  	msg->setText("FileSave");
-  	msg->exec();
+
 }
 
---
+++ src/FileHandler/FileHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by иван on 21.03.2022.
+//
+
+#include "FileHandler.h"
